c24a0942ff296c6522bdfc2405af51c22297afdd
bacc0 <37631578+bacc0@users.noreply.github.com>
2018-05-19 21:07:54 +0100

    Conditional-Statements-and-Loops-Exercises

--- 01-Conditional-Statements-and-Loops-Exercises/Calories-Counter/Program.cs
+++ 01-Conditional-Statements-and-Loops-Exercises/Calories-Counter/Program.cs
@@ -1,33 +1,35 @@
 ﻿using System;
 
-namespace cup
+namespace test_today
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+
             var calories = 0;
 
             for (int i = 0; i < n; i++)
             {
-                var ingredient = Console.ReadLine().ToLower();
+                var word = Console.ReadLine().ToLower();
 
-                if (ingredient == "cheese")
+                switch (word)
                 {
-                    calories += 500;
-                }
-                else if (ingredient == "tomato sauce")
-                {
-                    calories += 150;
-                }
-                else if (ingredient == "salami")
-                {
-                    calories += 600;
-                }
-                else if (ingredient == "pepper")
-                {
-                    calories += 50;
+                    case "cheese":
+                        calories += 500;
+                        break;
+                    case "tomato sauce":
+                        calories += 150;
+                        break;
+                    case "salami":
+                        calories += 600;
+                        break;
+                    case "pepper":
+                        calories += 50;
+                        break;
+                    default:
+                        break;
                 }
             }
             Console.WriteLine($"Total calories: {calories}");
