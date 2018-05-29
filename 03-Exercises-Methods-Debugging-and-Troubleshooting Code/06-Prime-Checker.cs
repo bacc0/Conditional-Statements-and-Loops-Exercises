41b3c0f0da4d7808d9828e4c6f74732f38278c18
bacc0 <37631578+bacc0@users.noreply.github.com>
2018-05-29 20:30:47 +0100

    Exercises-Methods-Debugging-and-Troubleshooting Code

---
+++ 03-Exercises-Methods-Debugging-and-Troubleshooting Code/06-Prime-Checker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace _06_PrimeChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            long inputNum = long.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(inputNum));
+        }
+
+        static bool IsPrime(long inputNum)
+        {
+            inputNum = Math.Abs(inputNum);
+            bool result = true;
+
+            if (inputNum == 2)
+            {
+                return true;
+            }
+            else if (inputNum == 1 || inputNum == 0)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(inputNum); i++)
+            {
+                if (inputNum % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ 03-Exercises-Methods-Debugging-and-Troubleshooting Code/14-Factorial-Trailing-Zeroes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class MainClass
+{
+    static void Main()
+    {
+        BigInteger num = BigInteger.Parse(Console.ReadLine());
+
+        BigInteger facturel = FactorialCalc(num);
+
+        Console.WriteLine(CountZeros(facturel));
+    }
+
+    static BigInteger FactorialCalc(BigInteger num)
+    {
+        BigInteger sum = num;
+        for (BigInteger i = num - 1; i >= 1; i--)
+        {
+            sum *= i;
+        }
+        return sum;
+    }
+
+    static BigInteger CountZeros(BigInteger facturel)
+    {
+        var counter = 0;
+        while (facturel > 0)
+        {
+            var temp = facturel % 10;
+
+            if (temp == 0)
+            {
+                counter++;
+            }
+            else
+            {
+                return counter;
+            }
+            facturel = facturel / 10;
+        }
+        return counter;
+    }
+}
+
+
