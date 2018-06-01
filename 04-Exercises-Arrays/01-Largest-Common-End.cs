578c0eef72bee89f8306b568ed14b825e3c132f3
Veselin Trayanov <bacco@Veselins-iMac.local>
2018-06-01 13:06:00 +0100

    Exercises-Arrays

---
+++ 04-Exercises-Arrays/01-Largest-Common-End.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsOne = Console.ReadLine().Split(' ').ToArray();
+            string[] wordsTwo = Console.ReadLine().Split(' ').ToArray();
+
+            int leftCount  = 0;
+            int rightCount = 0;
+
+
+            for (int i = 0; i < Math.Min(wordsOne.Length, wordsTwo.Length); i++)
+            {
+                if (wordsOne[i] == wordsTwo[i])
+                {
+                    leftCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < Math.Min(wordsOne.Length, wordsTwo.Length); i++)
+            {
+                if (wordsOne[wordsOne.Length - 1 - i] == wordsTwo[wordsTwo.Length - 1 - i])
+                {
+                    rightCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(Math.Max(leftCount, rightCount));
+        }
+    }
+}
