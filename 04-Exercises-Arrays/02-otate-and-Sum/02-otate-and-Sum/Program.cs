5680d93487019b36cbb2614c6a43f037c70e0bbe
Veselin Trayanov <bacco@Veselins-iMac.local>
2018-06-01 14:35:50 +0100

    3.Fold and Sum

---
+++ 04-Exercises-Arrays/02-otate-and-Sum/02-otate-and-Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_otate_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int intirationsNum = int.Parse(Console.ReadLine());
+
+
+            var sum = new int[nums.Length];
+
+            for (int i = 0; i < intirationsNum; i++)
+            {
+                var temp = nums[nums.Length - 1];
+                for (int j = nums.Length - 1 ; j> 0; j--)
+                {
+                    nums[j] = nums[j - 1];
+                }
+                nums[0] = temp;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    sum[j] += nums[j];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
---
+++ 04-Exercises-Arrays/03-Fold-and-Sum/03-Fold-and-Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int k = arr.Length / 4;
+
+            int[] left  = new int[k];
+            int[] mid   = new int[2 * k];
+            int[] right = new int[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                left[i]  = arr[i];
+                right[i] = arr[arr.Length - 1 - i];
+            }
+            Array.Reverse(left);
+
+            int[] upper = new int[2 * k];
+
+            for (int i = 0; i < k; i++)
+            {
+                upper[i]     = left[i];
+                upper[i + k] = right[i];
+            }
+
+            int[] sum = new int[2 * k];
+
+            for (int i = 0; i < 2 * k; i++)
+            {
+                mid[i] = arr[ k + i];
+                sum[i] = upper[i] + mid[i];
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
