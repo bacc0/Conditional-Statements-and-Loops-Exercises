26dd4ba8219b7ec16c50f44b483293976bbc5eec
Veselin Hr. Trayanov <1.1@gmx.com>
2018-06-02 18:14:43 +0100

    Add files via upload

---
+++ 04-Exercises-Arrays/01B-Largest-Common-End(with-Method).cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace test1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] arrOne = Console.ReadLine().Split();
+            string[] arrTwo = Console.ReadLine().Split();
+
+            var countLeft  = 0;
+            var countRight = 0;
+            int avarage = Math.Min(arrOne.Length, arrTwo.Length);
+
+            countLeft = ReturnCount(avarage, countLeft, arrOne, arrTwo);
+
+            Array.Reverse(arrOne);
+            Array.Reverse(arrTwo);
+
+            countRight = ReturnCount(avarage, countRight, arrOne, arrTwo);
+
+            Console.WriteLine(Math.Max(countLeft, countRight));
+        }
+
+        static int ReturnCount(int avarage, int count , string[] arrOne, string[] arrTwo)
+        {
+            for (int i = 0; i < avarage; i++)
+            {
+                if (arrOne[i] == arrTwo[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
