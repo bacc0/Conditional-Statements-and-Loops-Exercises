c16b12ae7793d66b15cd6baa3bc70b5a821712d4
Veselin Hr. Trayanov <1.1@gmx.com>
2018-06-02 14:00:33 +0100

    Add files via upload

---
+++ 04-Exercises-Arrays/09-ndex-of-Letters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ndexofLetters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var alphabet = new char[26];
+
+            for (char i = 'a'; i <='z'; i++)
+            {
+                alphabet[i - 'a'] = i;
+            }
+            string input = Console.ReadLine();
+
+            var arr = input.ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (input[i] == alphabet[j])
+                    {
+                        Console.WriteLine(input[i] + " -> " + j);
+                    }
+                }
+            }
+        }
+    }
+}
