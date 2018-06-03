0d8d0d447af21b249381db13688a04facc52a43c
Veselin Hr. Trayanov <bacco7@me.com>
2018-06-03 14:29:51 +0100

    late one

--- 04-Exercises-Arrays/09-ndex-of-Letters/Program.cs
+++ 04-Exercises-Arrays/09-ndex-of-Letters/Program.cs
@@ -1,30 +1,30 @@
-﻿using System;
+using System;
 using System.Linq;
-
-
+ 
+ 
 namespace ndexofLetters
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            var alphabet = new char[26];
-
-            for (char i = 'a'; i <='z'; i++)
+            char[] alphabet = new char[26];
+ 
+            for (char i = 'a'; i <= 'z' ; i++)
             {
                 alphabet[i - 'a'] = i;
             }
+ 
             string input = Console.ReadLine();
-
-            var arr = input.ToArray();
-
-            for (int i = 0; i < input.Length; i++)
+            var inputArr = input.ToArray();
+ 
+            for (int i = 0; i < inputArr.Length ; i++)
             {
-                for (int j = 0; j < alphabet.Length; j++)
+                for (int j = 0; j < alphabet.Length ; j++)
                 {
-                    if (input[i] == alphabet[j])
+                    if (inputArr[i] == alphabet[j])
                     {
-                        Console.WriteLine(input[i] + " -> " + j);
+                        Console.WriteLine($"{alphabet[j]} -> {j}");
                     }
                 }
             }
