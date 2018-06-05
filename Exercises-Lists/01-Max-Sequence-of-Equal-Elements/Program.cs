eb85e6a51c76cf02a6d4e029fa414d6a0481a219
Veselin Hr. Trayanov <1.1@gmx.com>
2018-06-05 13:14:00 +0100

    Add files via upload

---
+++ Exercises-Lists/01-Max-Sequence-of-Equal-Elements/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaxSequenceofEqualElements
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                       .Split(' ')
+                                       .Select(int.Parse)
+                                       .ToList();
+            int counter = 1;
+            int counterMax = 0;
+            int numberMax = 0;
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    counter++;
+                    if (counter > counterMax)
+                    {
+                        counterMax = counter;
+                        numberMax = numbers[i];
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+                if (i + 1 == numbers.Count - 1)
+                {
+                    if (counter > counterMax)
+                    {
+                        counterMax = counter;
+                        numberMax = numbers[i];
+                    }
+                }
+            }
+            for (int i = 0; i < counterMax; i++)
+            {
+                Console.Write(numberMax + " ");
+            }
+        }
+    }
+}
