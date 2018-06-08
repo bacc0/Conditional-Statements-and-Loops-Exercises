e90bd12c769af7cf566208654608f226317b6efc
beWater <bewater@MacBook-Pro.local>
2018-06-08 14:28:30 +0100

    04

---
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/04-Fix-Emails/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace FixEmails
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var mailBook = new Dictionary<string, string>();
+            string name = "";
+            string mail = "";
+            string commands =Console.ReadLine();
+
+            while (commands != "stop")
+            {
+                name = commands;
+                mail = Console.ReadLine();
+                if (!mailBook.ContainsKey(name))
+                {
+                    mailBook.Add(name, mail);
+                }
+                commands = Console.ReadLine();
+            }
+            foreach (var pair in mailBook
+                     .Where(x => !x.Value.EndsWith(".us") &&
+                                 !x.Value.EndsWith(".uk") 
+                           ) 
+                    )
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
