75c3fe646d34a48a597dcfade16915c61d15717c
beWater <bewater@MacBook-Pro.local>
2018-06-08 13:07:04 +0100

    01

--- 06-Exercises-Dictionaries-Lambda-and-LINQ/01-Phonebook/Program.cs
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/01-Phonebook/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Phonebook
 {
@@ -6,7 +9,41 @@ namespace Phonebook
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            string[] input = Console.ReadLine()
+                                    .Split(' ')
+                                    .ToArray();
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "A")
+                {
+                    if (!phonebook.ContainsKey(input[1]))
+                    {
+                        phonebook.Add(input[1], input[2]);
+                    }
+                    else
+                    {
+                        phonebook[input[1]] = input[2];
+                    }
+                }
+                else if (input[0] == "S")
+                {
+                    if (!phonebook.ContainsKey(input[1]))
+                    {
+                        Console.WriteLine($"Contact {input[1]} does not exist.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input[1]} -> {phonebook[input[1]]}");
+                    }
+                }
+
+                 input = Console.ReadLine()
+                                    .Split(' ')
+                                    .ToArray();
+            }
         }
     }
 }
---
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/02-Phonebook-Upgrade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhonebookUpgrade
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
