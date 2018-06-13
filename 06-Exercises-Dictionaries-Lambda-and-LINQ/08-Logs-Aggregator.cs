f16be89b6836f9c65acca8a0281c5a838450d3af
Veselin Hr. Trayanov <1.1@gmx.com>
2018-06-13 14:35:24 +0100

    Update 08-Logs-Aggregator.cs

--- 06-Exercises-Dictionaries-Lambda-and-LINQ/08-Logs-Aggregator.cs
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/08-Logs-Aggregator.cs
@@ -2,56 +2,48 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Logs-Aggregator
+namespace test2
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
             var nameIpsDurations = new SortedDictionary
-                              <string, SortedDictionary<string, int>>();
+                                 <string, SortedDictionary<string, int>>();
 
             var count = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < count; i++) 
+            for (int i = 0; i < count; i++)
             {
-                var line = Console.ReadLine();
+                var input = Console.ReadLine();
 
-                var split = line.Split()
-                                .ToArray();
+                var split = input.Split();
 
-                var nameUser = split[1];
-                var ip   = split[0];
-                var duration = int.Parse(split[2]);
-
+                var user = split[1];
+                var ipS  = split[0];
+                var seconds = int.Parse(split[2]);
 
 
-                if ( ! nameIpsDurations.ContainsKey(nameUser))
+                if ( ! nameIpsDurations.ContainsKey(user))
                 {
-                    nameIpsDurations[nameUser] = new SortedDictionary<string, int>();
+                    nameIpsDurations[user] = new SortedDictionary<string, int>();
                 }
-                if (! nameIpsDurations[nameUser].ContainsKey(ip))
+                if ( ! nameIpsDurations[user].ContainsKey(ipS))
                 {
-                    nameIpsDurations[nameUser][ip] = 0;
+                    nameIpsDurations[user][ipS] = 0;
                 }
-
-                nameIpsDurations[nameUser][ip] += duration;
+                nameIpsDurations[user][ipS] += seconds;
             }
+
 
             foreach (var nameIpsDuration in nameIpsDurations)
             {
                 var name = nameIpsDuration.Key;
-                var ipDuration = nameIpsDuration.Value;
+                var ipAdreses = nameIpsDuration.Value.Keys;
+                var secondsSum = nameIpsDuration.Value.Values.Sum();
 
-                var totalDurations = ipDuration.Sum( x => x.Value);
-
-                var ipS = ipDuration.Keys.ToArray();
-
-                Console.WriteLine($"{name}: {totalDurations} [{string.Join(", ", ipS)}]");
+                Console.WriteLine($"{name}: {secondsSum} [{string.Join(", ", ipAdreses)}]");
             }
         }
     }
 }
-
-        
-
