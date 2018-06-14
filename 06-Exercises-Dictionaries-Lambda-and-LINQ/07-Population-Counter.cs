691c34de6223753e79ded2f06d8f820c0d7c0af2
Veselin Hr. Trayanov <1.1@gmx.com>
2018-06-14 19:10:21 +0100

    Update 07-Population-Counter.cs

--- 06-Exercises-Dictionaries-Lambda-and-LINQ/07-Population-Counter.cs
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/07-Population-Counter.cs
@@ -2,58 +2,46 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-
-namespace Population=Counter
+namespace test2
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
             var countries = new Dictionary<string, Dictionary<string, long>>();
+            var input = Console.ReadLine().Split('|').ToArray();
 
-            var input = Console.ReadLine()
-                               .Split('|')
-                               .ToList();
-
-            var country = "";
             var town = "";
-            long population = 0;
-            
+            var state = "";
+            var population = 0;
+
             while (input[0] != "report")
             {
-                country = input[1];
                 town = input[0];
-                population = long.Parse(input[2]);
+                state = input[1];
+                population = int.Parse(input[2]);
 
-                if ( ! countries.ContainsKey(country))
-                {
-                    var townInfo = new Dictionary<string, long>();
-                    townInfo.Add(town, population);
-                    
-                    countries.Add(country, townInfo);
-                }
-                else
-                {
-                    if ( ! countries[country].ContainsKey(town))
-                    {
-                        countries[country].Add(town, population);
-                    }
-                }
 
-                input = Console.ReadLine()
-                               .Split('|')
-                               .ToList();
+                if ( ! countries.ContainsKey(state))
+                {
+                    countries[state] = new Dictionary<string, long>();
+                }
+                if ( ! countries[state].ContainsKey(town))
+                {
+                    countries[state][town] = 0;
+                }
+                countries[state][town] = population;
+
+
+                input = Console.ReadLine().Split('|').ToArray();
             }
 
-            foreach (var pair in countries
-                                       .OrderByDescending( 
-                                        x => x.Value.Values.Sum()))
+            
+            foreach (var pair in countries.OrderByDescending( x => x.Value.Values.Sum()))
             {
                 Console.WriteLine($"{pair.Key} (total population: {pair.Value.Values.Sum()})");
 
-                foreach (var pairTown in pair.Value
-                                             .OrderByDescending( 
-                                              y => y.Value))
+                foreach (var pairTown in pair.Value.OrderByDescending( y => y.Value))
                 {
                     Console.WriteLine($"=>{pairTown.Key}: {pairTown.Value}");
                 }
@@ -62,3 +50,4 @@ namespace Population=Counter
     }
 }
 
+        
