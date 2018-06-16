f78ffd25e9d18a23766d604a803056716a29bd33
veselin trayanov <bacco7@me.com>
2018-06-16 15:00:46 +0100

    new 07

---
+++ 08-Exercises-Strings-and-TextProcessing/03-Unicode-Characters/03-Unicode-Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UnicodeCharacters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            foreach (var item in input)
+            {
+               string result = GetEscape(item);
+               Console.Write(result);
+            }
+        }
+
+
+        public static string GetEscape(char c)
+        {
+            return "\\u" + ((int)c)
+                                .ToString("X4")
+                                .ToLower();
+        }
+    }
+}
