979201cdf524641481831ec76120635e29f40a2a
Veselin Hr. Trayanov <bacco7@me.com>
2018-07-18 11:03:51 +0100

    Update 08-Logs-Aggregator.cs

--- 06-Exercises-Dictionaries-Lambda-and-LINQ/08-Logs-Aggregator.cs
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/08-Logs-Aggregator.cs
@@ -1,49 +1,93 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace test2
+
+namespace Phonebook
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            var nameIpsDurations = new SortedDictionary
-                                 <string, SortedDictionary<string, int>>();
+            var userDuration = new SortedDictionary<string, SortedDictionary<string, int>>();
 
-            var count = int.Parse(Console.ReadLine());
+            var r = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < r; i++)
             {
-                var input = Console.ReadLine();
+                var input = Console.ReadLine().Split().ToList();
 
-                var split = input.Split();
+                var ipS = input[0];
+                var user = input[1];
+                var sec = int.Parse(input[2]);
 
-                var user = split[1];
-                var ipS  = split[0];
-                var seconds = int.Parse(split[2]);
-
-
-                if ( ! nameIpsDurations.ContainsKey(user))
+                if (!userDuration.ContainsKey(user))
                 {
-                    nameIpsDurations[user] = new SortedDictionary<string, int>();
+                    userDuration[user] = new SortedDictionary<string, int>();
                 }
-                if ( ! nameIpsDurations[user].ContainsKey(ipS))
+                if (!userDuration[user].ContainsKey(ipS))
                 {
-                    nameIpsDurations[user][ipS] = 0;
+                    userDuration[user][ipS] = 0;
                 }
-                nameIpsDurations[user][ipS] += seconds;
+                userDuration[user][ipS] += sec;
             }
 
-
-            foreach (var nameIpsDuration in nameIpsDurations)
+            foreach (var u in userDuration)
             {
-                var name = nameIpsDuration.Key;
-                var ipAdreses = nameIpsDuration.Value.Keys;
-                var secondsSum = nameIpsDuration.Value.Values.Sum();
-
-                Console.WriteLine($"{name}: {secondsSum} [{string.Join(", ", ipAdreses)}]");
+                Console.WriteLine($"{u.Key}: {u.Value.Values.Sum()} [{string.Join(", ",u.Value.Keys)}]");
             }
+
         }
     }
 }
+
+
+// using System;
+// using System.Linq;
+// using System.Collections.Generic;
+
+// namespace test2
+// {
+//     class MainClass
+//     {
+//         public static void Main(string[] args)
+//         {
+//             var nameIpsDurations = new SortedDictionary
+//                                  <string, SortedDictionary<string, int>>();
+
+//             var count = int.Parse(Console.ReadLine());
+
+//             for (int i = 0; i < count; i++)
+//             {
+//                 var input = Console.ReadLine();
+
+//                 var split = input.Split();
+
+//                 var user = split[1];
+//                 var ipS  = split[0];
+//                 var seconds = int.Parse(split[2]);
+
+
+//                 if ( ! nameIpsDurations.ContainsKey(user))
+//                 {
+//                     nameIpsDurations[user] = new SortedDictionary<string, int>();
+//                 }
+//                 if ( ! nameIpsDurations[user].ContainsKey(ipS))
+//                 {
+//                     nameIpsDurations[user][ipS] = 0;
+//                 }
+//                 nameIpsDurations[user][ipS] += seconds;
+//             }
+
+
+//             foreach (var nameIpsDuration in nameIpsDurations)
+//             {
+//                 var name = nameIpsDuration.Key;
+//                 var ipAdreses = nameIpsDuration.Value.Keys;
+//                 var secondsSum = nameIpsDuration.Value.Values.Sum();
+
+//                 Console.WriteLine($"{name}: {secondsSum} [{string.Join(", ", ipAdreses)}]");
+//             }
+//         }
+//     }
+// }
