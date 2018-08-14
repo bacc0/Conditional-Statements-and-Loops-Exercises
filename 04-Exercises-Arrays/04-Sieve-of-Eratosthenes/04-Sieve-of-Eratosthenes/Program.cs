510ed2fee41f21f4747cfefc0b1e56dc7a96f028
Veselin Trayanov <bacco@Veselins-iMac.local>
2018-08-14 21:51:32 +0100

    1

--- 04-Exercises-Arrays/04-Sieve-of-Eratosthenes/04-Sieve-of-Eratosthenes/Program.cs
+++ 04-Exercises-Arrays/04-Sieve-of-Eratosthenes/04-Sieve-of-Eratosthenes/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+
 
 namespace _04_Sieve_Оf_Eratosthenes
 {
@@ -10,46 +9,71 @@ namespace _04_Sieve_Оf_Eratosthenes
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            var players = new Dictionary<string, List<string>>();
 
-            int[] arrInput = new int[n + 1];
-            bool[] checkNums = new bool[n + 1];
-
-            string primeNums = null;
-
-            for (int i = 0; i <= n; i++)
+            while (true)
             {
-                arrInput[i] = i;
-                checkNums[i] = true;
-            }
+                var line = Console.ReadLine();
 
-            primeNums = SieveOfErat(arrInput, checkNums, primeNums);
-
-            Console.WriteLine(primeNums.Trim());
-        }
-
-        private static string SieveOfErat(int[] arrInput, bool[] checkNums, string primeNums)
-        {
-            checkNums[0] = false;
-            checkNums[1] = false;
-
-            for (int i = 0; i < arrInput.Length; i++)
-            {
-                if (checkNums[i])
+                if (line == "JOKER")
                 {
-                    primeNums += $"{arrInput[i]} ";
-
-                    for (int j = i + 1; j < arrInput.Length; j++)
-                    {
-                        if (arrInput[j] % i == 0 && checkNums[j] == true)
-                        {
-                            checkNums[j] = false;
-                        }
-                    }
+                    break;
                 }
+
+                var tokens = line.Split(':');
+
+                var name  = tokens[0];
+                var cards = tokens[1]
+                                .Split(new char[] { ' ', ',' }
+                                       ,StringSplitOptions
+                                       .RemoveEmptyEntries);
+
+                if ( ! players.ContainsKey(name))
+                {
+                    players[name] = new List<string>();
+                }
+                players[name].AddRange(cards);
             }
 
-            return primeNums;
+            var powers = new Dictionary<string, int>();
+
+            for (int i = 2; i < 10; i++)
+            {
+                powers.Add(i.ToString(), i);
+            }
+            powers.Add("1", 10);  
+
+            powers.Add("J", 11);  
+            powers.Add("Q", 12);  
+            powers.Add("K", 13);  
+            powers.Add("A", 14); 
+
+            powers.Add("C", 1);  
+            powers.Add("D", 2);
+            powers.Add("H", 3);
+            powers.Add("S", 4);
+
+            foreach (var player in players)
+            {
+                var cards = player.Value
+                                  .Distinct()
+                                  .ToList();
+                var sum = 0;
+
+                foreach (var card in cards)
+                {
+                    var cardPowerStr = card[0].ToString();
+                    var cardColorStr = card[card.Length - 1].ToString();
+
+                    int cardPower = powers[cardPowerStr];
+                    int cardColor = powers[cardColorStr];
+
+                    sum += cardPower * cardColor;
+
+                }
+                Console.WriteLine($"{player.Key}: {sum}");
+            }
         }
     }
 }
+  
