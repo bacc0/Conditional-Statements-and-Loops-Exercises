fb6e29762a02f1453a50618432df8b7890f3bac0
bacc0 <1.1@gmx.com>
2018-08-26 00:15:02 +0100

    .

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace test
 {
@@ -6,7 +8,41 @@ namespace test
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .ToArray();
+
+            var numbersoOfRotations = int.Parse(Console.ReadLine());
+
+            var sum = new int[numbers.Length];
+
+            var rotated = new int[numbers.Length];
+
+
+
+            for (int i = 0; i < numbersoOfRotations; i++) 
+            {
+
+                rotated = Rotated(numbers);
+                    
+                for (int j = 0; j < numbers.Length; j++) 
+                {
+                    sum[j] += rotated[i];
+                }
+            }
+        }
+
+        private static int[] Rotated(int[] numbers)
+        {
+            var rotated = new int[numbers.Length];
+
+            for (int i = 1; i < rotated.Length; i++) 
+            {
+                rotated[i] = numbers[i - 1];
+            }
+
+            return rotated;
         }
     }
 }
