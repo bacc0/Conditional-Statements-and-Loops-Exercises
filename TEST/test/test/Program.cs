8cc95ed0704ba70aed5bc7a4c58b6275bdf59dab
bacc0 <1.1@gmx.com>
2018-08-26 02:15:59 +0100

    .

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -8,6 +8,7 @@ namespace test
     {
         public static void Main(string[] args)
         {
+
             var arr = Console.ReadLine()
                              .Split(' ')
                              .Select(int.Parse)
@@ -15,30 +16,33 @@ namespace test
 
             var k = arr.Length / 4;
 
-            var uppeerArr = new int[k * 2];
-            var lowerArr  = new int[k * 2];
+            var arrDown = new int[k * 2]; 
+            var arrUp = new int[k * 2]; 
 
-            for (int i = 0; i < k * 2; i++) 
+            for (int i = 0; i < arrDown.Length ; i++) 
             {
-                lowerArr[i] = arr[i + k];
+                arrDown[i] = arr[k + i];
             }
 
             for (int i = 0; i < k; i++)
             {
-                uppeerArr[i] = arr[k - 1 - i];
-                uppeerArr[k + i] = arr[arr.Length - 1 - i];
+                arrUp[i] = arr[k - 1 -i];
+                arrUp[ k + i ] = arr[arr.Length - 1 - i];
             }
 
             var sum = new int[k * 2]; 
 
             for (int i = 0; i < sum.Length; i++) 
             {
-                sum[i] = uppeerArr[i] + lowerArr[i]; 
+                sum[i] = arrUp[i] + arrDown[i];
             }
-
-            Console.WriteLine(String.Join(" ", sum));
-
+            Console.WriteLine(string.Join(" ", arrUp));
+            Console.WriteLine(string.Join(" ", arrDown));
+            Console.WriteLine(string.Join(" ", sum));
 
         }
     }
 }
+
+
+//     1 2 3 4 5 6 7 8
