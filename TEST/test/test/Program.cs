81302fde8e5d7100bc06ef5be1910d61e8bba607
Veselin Trayanov <bacco7@me.com>
2018-08-27 13:45:05 +0100

    .

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -7,9 +7,8 @@ namespace test
 {
     class MainClass
     {
-        private static void Main(string[] args)
+        static void Main()
         {
-
             List<int> numbers = Console.ReadLine()
                                        .Split()
                                        .Select(int.Parse)
@@ -24,6 +23,7 @@ namespace test
                 if (numbers[i] == numbers[i + 1])
                 {
                     count++;
+
                     if (maxCount < count)
                     {
                         maxCount = count;
@@ -34,7 +34,13 @@ namespace test
                 {
                     count = 1;
                 }
+                if (maxCount < count)
+                {
+                    maxCount = count;
+                    maxNum = numbers[i];
+                }
             }
+
             for (int i = 0; i < maxCount; i++)
             {
                 Console.Write($"{maxNum} ");
@@ -44,4 +50,34 @@ namespace test
 }
 
 
-//     3 4 4 5 5 5 2 2
+//   01.
+//      List<int> numbers = Console.ReadLine()
+//                                             .Split()
+//                                             .Select(int.Parse)
+//                                             .ToList();
+                  //      var count = 1;
+//      var maxCount = 0;
+//      var maxNum = 0;
+                  //      for (int i = 0; i<numbers.Count - 1; i++)
+//      {
+//          if (numbers[i] == numbers[i + 1])
+//          {
+//              count++;
+                  //              if (maxCount<count)
+//              {
+//                  maxCount = count;
+//                  maxNum = numbers[i];
+//              }
+//          }
+//          else
+//          {
+//              count = 1;
+//          }
+//      }
+//      for (int i = 0; i<maxCount; i++)
+//      {
+//          Console.Write($"{maxNum} ");
+//      }
+
+
+
