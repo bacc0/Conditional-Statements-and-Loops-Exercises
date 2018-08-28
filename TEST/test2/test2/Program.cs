e8a47cfaed89aca2df101ec4b82ed2e59c0106b9
bacc0 <1.1@gmx.com>
2018-08-28 23:39:42 +0100

    `

--- TEST/test2/test2/Program.cs
+++ TEST/test2/test2/Program.cs
@@ -8,54 +8,174 @@ namespace test2
     {
         public static void Main(string[] args)
         {
-            var inp = Console.ReadLine();
-
-
             var dic = new SortedDictionary<string, Dictionary<string, int>>();
 
+            var interations = int.Parse(Console.ReadLine());
 
-            while (!inp.Equals("end"))
+            for (int i = 0; i < interations; i++) 
             {
+                var input = Console.ReadLine()
+                                   .Split()
+                                   .ToArray();
 
-                var input = inp.Split()
-                               .ToArray();
+                var user = input[1];
 
-                var ipFull = input[0];
-                var userFull = input[2];
+                var userIp = input[0];
+                var duration = int.Parse(input[2]);
 
-                var ipHelper = ipFull.Split('=').ToArray();
-                var userHelper = userFull.Split('=').ToArray();
-
-                var ip = ipHelper[1];
-                var user = userHelper[1];
-
-                if (!dic.ContainsKey(user))
+                if ( !dic.ContainsKey(user))
                 {
                     dic[user] = new Dictionary<string, int>();
                 }
-                if (!dic[user].ContainsKey(ip))
+                if ( !dic[user].ContainsKey(userIp))
                 {
-                    dic[user][ip] = 0;
+                    dic[user][userIp] = 0;
                 }
-
-                dic[user][ip] += 1;
-
-                inp = Console.ReadLine();
+                dic[user][userIp] += duration;
             }
 
-            foreach (var user in dic)
+            foreach (var userName in dic)
             {
-                Console.WriteLine(user.Key + ":");
+                Console.Write(userName.Key + ": ");
 
 
-                var result = "";
+                var ipS = new List<string>();
+                var sum = 0;
 
-                foreach (var pair in user.Value)
+                foreach (var ipD  in userName.Value.OrderBy(x => x.Key))
                 {
-                    result += $"{pair.Key} => {pair.Value}, ";
+                    ipS.Add(ipD.Key);
+
+                    sum += ipD.Value;
                 }
-                Console.WriteLine(result.Trim(new char[] {' ', ','}) + '.');
+                Console.Write(sum + " ");
+
+                Console.WriteLine("[" + string.Join(", ", ipS) + "]");
             }
         }
     }
 }
+
+
+
+//01.
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace test2
+//{
+//    class MainClass
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var inp = Console.ReadLine();
+
+
+//            var dic = new SortedDictionary<string, Dictionary<string, int>>();
+
+
+//            while (!inp.Equals("end"))
+//            {
+
+//                var input = inp.Split()
+//                               .ToArray();
+
+//                var ipFull = input[0];
+//                var userFull = input[2];
+
+//                var ipHelper = ipFull.Split('=').ToArray();
+//                var userHelper = userFull.Split('=').ToArray();
+
+//                var ip = ipHelper[1];
+//                var user = userHelper[1];
+
+//                if (!dic)
+//                {
+//                    dic[user] = new Dictionary<string, int>();
+//                }
+//                if (!dic[user].ContainsKey(ip))
+//                {
+//                    dic[user][ip] = 0;
+//                }
+
+//                dic[user][ip] += 1;
+
+//                inp = Console.ReadLine();
+//            }
+
+//            foreach (var user in dic)
+//            {
+//                Console.WriteLine(user.Key + ":");
+
+
+//                var result = "";
+
+//                foreach (var pair in user.Value)
+//                {
+//                    result += $"{pair.Key} => {pair.Value}, ";
+//                }
+//                Console.WriteLine(result.Trim(new char[] { ' ', ',' }) + '.');
+//            }
+//        }
+//    }
+//}
+
+
+
+
+// 02.
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace test2
+//{
+//    class MainClass
+//    {
+//        public static void Main(string[] args)
+//        {
+
+//            var dic = new Dictionary<string, Dictionary<string, long>>();
+
+//            while (true)
+//            {
+//                var input = Console.ReadLine();
+
+//                if (input.Equals("report"))
+//                {
+//                    break;
+//                }
+//                var elements = input.Split('|').ToArray();
+
+//                var capital = elements[0];
+//                var state = elements[1];
+//                var population = long.Parse(elements[2]);
+
+//                if (!dic.ContainsKey(state))
+//                {
+//                    dic[state] = new Dictionary<string, long>();
+//                }
+//                if (!dic[state].ContainsKey(capital))
+//                {
+//                    dic[state][capital] = 0;
+//                }
+
+//                dic[state][capital] += population;
+//            }
+
+//            foreach (var pair in dic.OrderByDescending(x => x.Value.Values.Sum()))
+//            {
+//                Console.WriteLine($"{pair.Key} (total population: {pair.Value.Values.Sum()})");
+
+//                foreach (var town in pair.Value.OrderByDescending(y => y.Value))
+//                {
+//                    Console.WriteLine($"=>{town.Key}: {town.Value}");
+//                }
+//            }
+//        }
+//    }
+//}
+
