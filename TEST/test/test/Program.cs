7f0cfef729ee8b81016e04874a6f1a9aee77fc6f
Veselin Trayanov <bacco7@me.com>
2018-08-29 14:45:19 +0100

    `

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿namespace test
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
 
 
 
