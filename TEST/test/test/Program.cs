38a5495c7e0b992cd477ea898ad622792e1923a9
bacc0 <1.1@gmx.com>
2018-09-19 00:09:22 +0100

    `

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -2,121 +2,186 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-
-
 namespace test
 {
     class MainClass
     {
         public static void Main()
         {
-           
-            string input = Console.ReadLine();
 
-            var houseOfCards = new Dictionary<string, Dictionary<int, HashSet<int>>>();
+            var input = Console.ReadLine();
+            var dic = new SortedDictionary<string, Dictionary<string, int>>();
 
-            while (input != "JOKER")
+            while (input != "end")
             {
-                string[] handInfo = input.Split(new char[] { ':', ',', ' ' }
-                                                ,StringSplitOptions
-                                                .RemoveEmptyEntries);
+                var data = input.Split();
 
-                string name = handInfo[0];
+                var userInput = data[2].Split('=');
+                var users = userInput[1];
 
-                if(!houseOfCards.ContainsKey(name))
+                var IpInput = data[0].Split('=');
+                var IPs = IpInput[1];
+
+                if (!dic.ContainsKey(users))
                 {
-                    houseOfCards.Add(name, new Dictionary<int, HashSet<int>>());
-
-                    for (int i = 1; i <= 4; i++)
-                    {
-                        houseOfCards[name].Add(i, new HashSet<int>());
-                    }
+                    dic[users] = new Dictionary<string, int>();
+                }
+                if (!dic[users].ContainsKey(IPs))
+                {
+                    dic[users][IPs] = 0;
                 }
 
-                for (int i = 1; i < handInfo.Length; i++)
-                {
-                    string currentCard = handInfo[i].Trim();
-                    int face = 0;
-                    int suite = 0;
-
-
-                    if (currentCard.Length > 2)
-                    {
-                         face = getFace(currentCard.Substring(0, 2));
-
-                         suite = getSuite(currentCard.Substring(2));
-                    }
-                    else
-                    {
-                         face = getFace(currentCard[0].ToString());
-
-                         suite = getSuite(currentCard[1].ToString());
-                    }
-
-                 
-
-                    if (!houseOfCards[name][suite].Contains(face))
-                    {
-
-                        houseOfCards[name][suite].Add(face);
-                    }
-                }
+                dic[users][IPs] += 1;
 
                 input = Console.ReadLine();
             }
 
-            foreach (var outerPair in houseOfCards)
-            {
-                int sum = 0;
+            //foreach (var user in dic)
+            //{
+            //    Console.WriteLine(user.Key + ":");
 
-                foreach (var innerPair in outerPair.Value)
+            //    var result = "";
+
+            //    foreach (var pair in user.Value)
+            //    {
+            //        result += $"{pair.Key} => {pair.Value}, \n";
+            //    }
+            //    Console.WriteLine(result.TrimEnd(new Char[] { ' ', ',', '\n' }) + ".");
+            //}
+
+            foreach (var user in dic)
+            {
+                Console.WriteLine(user.Key + ":");
+
+                var result = "";
+
+                foreach (var pair in user.Value)
                 {
-                    sum += innerPair.Key * innerPair.Value.Sum();
+                    result += $"{pair.Key} => {pair.Value}, ";
                 }
-                Console.WriteLine($"{outerPair.Key}: {sum}");
-            }
 
-
-
-
-        }
-        private static int getFace(string face)
-        {
-            switch (face)
-            {
-                case "J":
-                    return 11;
-                case "Q":
-                    return 12;
-                case "K":
-                    return 13;
-                case "A":
-                    return 14;
-                default:
-                    return int.Parse(face);
+                Console.WriteLine(result.TrimEnd(new Char[] { ' ', ',' }) + ".");
             }
         }
-        private static int getSuite(string suite)
-        {
-            switch (suite)
-            {
-                case "S":
-                    return 4;
-                case "H":
-                    return 3;
-                case "D":
-                    return 2;
-                case "C":
-                    return 1;
-                default:
-                    return 0;
-            }
-                                
-        }
-
-       
     }
 }
+
+
+
+
+//namespace test
+//{
+//    class MainClass
+//    {
+//        public static void Main()
+//        {
+           
+//            string input = Console.ReadLine();
+
+//            var houseOfCards = new Dictionary<string, Dictionary<int, HashSet<int>>>();
+
+//            while (input != "JOKER")
+//            {
+//                string[] handInfo = input.Split(new char[] { ':', ',', ' ' }
+//                                                ,StringSplitOptions
+//                                                .RemoveEmptyEntries);
+
+//                string name = handInfo[0];
+
+//                if(!houseOfCards.ContainsKey(name))
+//                {
+//                    houseOfCards.Add(name, new Dictionary<int, HashSet<int>>());
+
+//                    for (int i = 1; i <= 4; i++)
+//                    {
+//                        houseOfCards[name].Add(i, new HashSet<int>());
+//                    }
+//                }
+
+//                for (int i = 1; i < handInfo.Length; i++)
+//                {
+//                    string currentCard = handInfo[i].Trim();
+//                    int face = 0;
+//                    int suite = 0;
+
+
+//                    if (currentCard.Length > 2)
+//                    {
+//                         face = getFace(currentCard.Substring(0, 2));
+
+//                         suite = getSuite(currentCard.Substring(2));
+//                    }
+//                    else
+//                    {
+//                         face = getFace(currentCard[0].ToString());
+
+//                         suite = getSuite(currentCard[1].ToString());
+//                    }
+
+                 
+
+//                    if (!houseOfCards[name][suite].Contains(face))
+//                    {
+
+//                        houseOfCards[name][suite].Add(face);
+//                    }
+//                }
+
+//                input = Console.ReadLine();
+//            }
+
+//            foreach (var outerPair in houseOfCards)
+//            {
+//                int sum = 0;
+
+//                foreach (var innerPair in outerPair.Value)
+//                {
+//                    sum += innerPair.Key * innerPair.Value.Sum();
+//                }
+//                Console.WriteLine($"{outerPair.Key}: {sum}");
+//            }
+
+
+
+
+//        }
+//        private static int getFace(string face)
+//        {
+//            switch (face)
+//            {
+//                case "J":
+//                    return 11;
+//                case "Q":
+//                    return 12;
+//                case "K":
+//                    return 13;
+//                case "A":
+//                    return 14;
+//                default:
+//                    return int.Parse(face);
+//            }
+//        }
+//        private static int getSuite(string suite)
+//        {
+//            switch (suite)
+//            {
+//                case "S":
+//                    return 4;
+//                case "H":
+//                    return 3;
+//                case "D":
+//                    return 2;
+//                case "C":
+//                    return 1;
+//                default:
+//                    return 0;
+//            }
+                                
+//        }
+
+       
+//    }
+//}
 
 // 1 2 3 4 5
 // + 1 2 3 4
