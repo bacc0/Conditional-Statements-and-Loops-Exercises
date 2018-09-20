794f327127452830be9c18733fba11befc49182f
Veselin Trayanov <bacco7@me.com>
2018-09-20 18:19:30 +0100

    `

--- TEST/test/test/Program.cs
+++ TEST/test/test/Program.cs
@@ -8,67 +8,20 @@ namespace test
     {
         public static void Main()
         {
-<<<<<<< HEAD
-            var input = Console.ReadLine();
-=======
-            var data = Console.ReadLine();
->>>>>>> 405b14fa987cfa27095e2a73b7d2da86d4c534ed
 
-            var totalPopulation = new Dictionary<string, Dictionary<string, long>>();
+            var test1 = "a";
+            var test2 = "A";
 
-            while (data != "report")
-            {
-<<<<<<< HEAD
-                var handInfo = input.Split(new char[] { ':', ',', ' ' }
-                                    ,StringSplitOptions
-                                    .RemoveEmptyEntries);
 
-                var name = handInfo[0];
-
-                if ( !houseOfCards.ContainsKey(name))
-                {
-                    houseOfCards.Add(name, new Dictionary<int, HashSet<int>>());
-=======
-                var input = data.Split('|');
-
-                var state = input[1];
-                var town = input[0];
-                var population = long.Parse(input[2]);
-
->>>>>>> 405b14fa987cfa27095e2a73b7d2da86d4c534ed
-
-                if (!totalPopulation.ContainsKey(state))
-                {
-                    totalPopulation[state] = new Dictionary<string, long>();
-                }
-                if (!totalPopulation[state].ContainsKey(town))
-                {
-                    totalPopulation[state][town] = 0;
-                }
-
-                totalPopulation[state][town] += population;
-
-                data = Console.ReadLine();
-            }
-
-            foreach (var country in totalPopulation.OrderByDescending(x => x.Value.Values.Sum()))
-            {
-                Console.WriteLine($"{country.Key} (total population: {country.Value.Values.Sum()})");
-
-                foreach (var townAndPopulation in country.Value.OrderByDescending(x => x.Value))
-                {
-<<<<<<< HEAD
-                    var currentCard = handInfo[i]; //.Trim();
-
-                    int face = 0;
-                    int suite = 0;
-=======
-                    Console.WriteLine($"=>{townAndPopulation.Key}: {townAndPopulation.Value}");
-                }
-            }
+            Console.WriteLine(test1.CompareTo(test2));
         }
     }
 }
+
+
+    //using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
 //namespace test
 //{
@@ -76,87 +29,155 @@ namespace test
 //    {
 //        public static void Main()
 //        {
->>>>>>> 405b14fa987cfa27095e2a73b7d2da86d4c534ed
+
+//            var input = Console.ReadLine();
+
+//            var data = Console.ReadLine();
+
+
+//            var totalPopulation = new Dictionary<string, Dictionary<string, long>>();
+
+//            while (data != "report")
+//            {
+
+//                var handInfo = input.Split(new char[] { ':', ',', ' ' }
+//                                    ,StringSplitOptions
+//                                    .RemoveEmptyEntries);
+
+//                var name = handInfo[0];
+
+//                if ( !houseOfCards.ContainsKey(name))
+//                {
+//                    houseOfCards.Add(name, new Dictionary<int, HashSet<int>>());
+
+//                var input = data.Split('|');
+
+//                var state = input[1];
+//                var town = input[0];
+//                var population = long.Parse(input[2]);
+
+
+
+//                if (!totalPopulation.ContainsKey(state))
+//                {
+//                    totalPopulation[state] = new Dictionary<string, long>();
+//                }
+//                if (!totalPopulation[state].ContainsKey(town))
+//                {
+//                    totalPopulation[state][town] = 0;
+//                }
+
+//                totalPopulation[state][town] += population;
+
+//                data = Console.ReadLine();
+//            }
+
+//            foreach (var country in totalPopulation.OrderByDescending(x => x.Value.Values.Sum()))
+//            {
+//                Console.WriteLine($"{country.Key} (total population: {country.Value.Values.Sum()})");
+
+//                foreach (var townAndPopulation in country.Value.OrderByDescending(x => x.Value))
+//                {
+
+//                    var currentCard = handInfo[i]; //.Trim();
+
+//                    int face = 0;
+//                    int suite = 0;
+
+//                    Console.WriteLine($"=>{townAndPopulation.Key}: {townAndPopulation.Value}");
+//                }
+//            }
+//        }
+//    }
+//}
+
+//namespace test
+//{
+//    class MainClass
+//    {
+//        public static void Main()
+//        {
+
 
 //            var input = Console.ReadLine();
 //            var dic = new SortedDictionary<string, Dictionary<string, int>>();
 
-<<<<<<< HEAD
-                    if (currentCard.Length > 2)
-                    {
-                        face = GetFace(currentCard.Substring(0, 2));
 
-                        suite = GetSuite(currentCard.Substring(2));
-                    }
-                    else
-                    {
-                        face = GetFace(currentCard[0].ToString());
+//                    if (currentCard.Length > 2)
+//                    {
+//                        face = GetFace(currentCard.Substring(0, 2));
 
-                        suite = GetSuite(currentCard[1].ToString());
-                    }
+//                        suite = GetSuite(currentCard.Substring(2));
+//                    }
+//                    else
+//                    {
+//                        face = GetFace(currentCard[0].ToString());
 
-                    if (!houseOfCards[name][suite].Contains(face))
-                    {
-                        houseOfCards[name][suite].Add(face);
-                    }
-                }
+//                        suite = GetSuite(currentCard[1].ToString());
+//                    }
 
-
-                input = Console.ReadLine();
-            }
-
-            foreach (var outerPair in houseOfCards)
-            {
-                Console.Write(outerPair.Key + ": ");
-
-                var sum = 0;
-                foreach (var innerPair in outerPair.Value)
-                {
-                   sum += innerPair.Key * innerPair.Value.Sum();
-                }
-                Console.WriteLine(sum);
-            }
+//                    if (!houseOfCards[name][suite].Contains(face))
+//                    {
+//                        houseOfCards[name][suite].Add(face);
+//                    }
+//                }
 
 
-        }
-        private static int GetFace(string face)
-        {
-            switch (face)
-            {
-                case "J":
-                    return 11;
-                case "Q":
-                    return 12;
-                case "K":
-                    return 13;
-                case "A":
-                    return 14;
-                default:
-                    return int.Parse(face);
-            }
-        }
+//                input = Console.ReadLine();
+//            }
 
-        private static int GetSuite(string suite)
-        {
-            switch (suite)
-            {
-                case "S":
-                    return 4;
-                case "H":
-                    return 3;
-                case "D":
-                    return 2;
-                case "C":
-                    return 1;
-                default :
-                    return 0;
-            }
-        }
+//            foreach (var outerPair in houseOfCards)
+//            {
+//                Console.Write(outerPair.Key + ": ");
+
+//                var sum = 0;
+//                foreach (var innerPair in outerPair.Value)
+//                {
+//                   sum += innerPair.Key * innerPair.Value.Sum();
+//                }
+//                Console.WriteLine(sum);
+//            }
 
 
-    }
-}
-=======
+//        }
+//        private static int GetFace(string face)
+//        {
+//            switch (face)
+//            {
+//                case "J":
+//                    return 11;
+//                case "Q":
+//                    return 12;
+//                case "K":
+//                    return 13;
+//                case "A":
+//                    return 14;
+//                default:
+//                    return int.Parse(face);
+//            }
+//        }
+
+//        private static int GetSuite(string suite)
+//        {
+//            switch (suite)
+//            {
+//                case "S":
+//                    return 4;
+//                case "H":
+//                    return 3;
+//                case "D":
+//                    return 2;
+//                case "C":
+//                    return 1;
+//                default :
+//                    return 0;
+//            }
+//        }
+
+
+//    }
+//}
+
 //            while (input != "end")
 //            {
 //                var data = input.Split();
@@ -312,7 +333,7 @@ namespace test
        
 //    }
 //}
->>>>>>> 405b14fa987cfa27095e2a73b7d2da86d4c534ed
+
 
 // 1 2 3 4 5
 // + 1 2 3 4
