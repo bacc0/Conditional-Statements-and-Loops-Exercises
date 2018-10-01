fb559f67de8e3a63ebb4e0db8be92f3d0a28b444
Veselin Trayanov <bacco7@me.com>
2018-10-01 15:26:37 +0100

    `

---
+++ 06-Exercises-Dictionaries-Lambda-and-LINQ/B-01_AveraageMarks/B-01_AveraageMarks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace B01_AveraageMarks
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var numberOfStudents = int.Parse(Console.ReadLine());
+
+            var students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                var entry = Console.ReadLine()
+                                   .Split(new char[] { ' ' }
+                                   , StringSplitOptions.RemoveEmptyEntries); 
+
+                var student = entry[0];
+                var marks = double.Parse(entry[1]);
+
+
+                if (!students.ContainsKey(student))
+                {
+                    students[student] = new List<double>();
+                }
+                else
+                {
+                    students[student].Add(marks);
+                }
+            }
+
+            Console.WriteLine("jj");
+
+        }
+    }
+}
